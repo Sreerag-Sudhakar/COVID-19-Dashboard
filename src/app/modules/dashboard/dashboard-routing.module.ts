bd684db3ec974fc09c62b6ec65a41548d6de2f49
Sreerag <sreerag@alignminds.net>
2021-05-10 00:44:11 +0530

    Styles updates and auth guard added in dashboard modules

--- src/app/modules/dashboard/dashboard-routing.module.ts
+++ src/app/modules/dashboard/dashboard-routing.module.ts
@@ -4,13 +4,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { DashLayoutComponent } from './components/dash-layout/dash-layout.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { CountryComponent } from './components/country/country.component';
-
+import { AuthGuard } from '../../core/guards/auth/auth.guard'
 const routes: Routes = [
   {
     path : '',
     component : DashLayoutComponent,
     children : [
-      { path : '', component : DashboardComponent},
+      { path : '', component : DashboardComponent, canActivate: [AuthGuard]},
       { path : 'country', component : CountryComponent}
     ]
   }
