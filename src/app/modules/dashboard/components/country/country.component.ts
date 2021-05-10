f42e44c9bd7c4e851db2a2b15c4bc50ea814f6d1
Sreerag <sreerag@alignminds.net>
2021-05-10 13:58:15 +0530

    Pagination implemented

--- src/app/modules/dashboard/components/country/country.component.ts
+++ src/app/modules/dashboard/components/country/country.component.ts
@@ -12,6 +12,11 @@ export class CountryComponent implements OnInit {
   sortKey:string = 'country';
   searchKey:string = '';
   countriesData:CountryData[] = []
+  pager = {
+    currentPage: 1,
+    itemsPerPage: 30,
+    totalItems:0
+  }
   constructor(private countryService: CountryService) { }
 
   ngOnInit(): void {
@@ -20,9 +25,10 @@ export class CountryComponent implements OnInit {
 
   getCountryWiseStatistics(): void {
     this.countryService.getCountryWiseStatistics().subscribe((data: any) => {
-      this.countries = <CountryData[]>data;
+      // this.countries = <CountryData[]>data;
       console.log(this.countries)
-      this.countriesData = [...this.countries]
+      this.countriesData = [...data]
+      this.getResultsByFilters();
     });  
     
   }
@@ -47,5 +53,42 @@ export class CountryComponent implements OnInit {
       return country.country.toLowerCase().includes(searchKey.toLowerCase())
     })
     console.log(this.countries)
+  }
+
+  getResultsByFilters() {
+    let results:CountryData[] = [... this.countriesData];
+    if(this.searchKey.trim()) {
+      results = this.countriesData.filter((country:CountryData)=>{
+        return country.country.toLowerCase().includes(this.searchKey.toLowerCase())
+      })
+    }
+    this.pager.totalItems = results.length;
+    if(this.sortKey){
+      if(this.sortKey == 'country')
+      results = results.sort((a:any, b:any)=>{
+        return a[this.sortKey].localeCompare(b[this.sortKey]);
+      })
+      else
+      results = results.sort((a:any, b:any)=>{
+        return a[this.sortKey] - b[this.sortKey];
+      })
+    }
+
+    let startIndex = (this.pager.currentPage - 1) * this.pager.itemsPerPage;
+    let endIndex = startIndex + this.pager.itemsPerPage;
+    this.countries = results.slice(startIndex, endIndex)
+  }
+
+  previous () {
+    if(this.pager.currentPage != 1)
+    this.pager.currentPage--;
+    this.getResultsByFilters()
+  }
+
+  next () {
+    let totalPages = Math.ceil(this.countriesData.length/this.pager.itemsPerPage);
+    if(this.pager.currentPage < totalPages)
+    this.pager.currentPage++;
+    this.getResultsByFilters()
   }
 }
